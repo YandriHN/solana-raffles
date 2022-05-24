acf82792cd7f829d7b0d12b91f48185f7fd42549
alby <alby.dev@protonmail.com>
2022-05-24 22:01:19 +0100

    typescript dependencies

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -18,20 +18,12 @@ import { Toaster } from "react-hot-toast";
 import { Menu } from "../src/components/menu/menu";
 import { ContractProvider } from "../src/context/contract";
 
-// Use require instead of import since order matters
 require("@solana/wallet-adapter-react-ui/styles.css");
 require("../styles/globals.scss");
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
-  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
   const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
